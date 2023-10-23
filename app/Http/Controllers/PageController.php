ebd0df76d1b55522e28334de1aae75e04f7eaf1a
vicSystems <victorasuquob@gmail.com>
2023-10-23 12:26:15 +0100

    logo update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Artesaos\SEOTools\Facades\OpenGraph;
 
 use Illuminate\Http\Request;
 
@@ -8,6 +10,18 @@ class PageController extends Controller
 {
     //
     public function index(){
+
+        SEOTools::setTitle('Jafro Foods Ltd.');
+        SEOTools::setDescription("Explore the finest African chin chin at Jafro Foods. Discover authentic flavors and quality in every bite. Your journey to the heart of Africa's taste begins here.");
+        // SEOTools::opengraph()->setUrl('http://current.url.com');
+        // SEOTools::setCanonical('https://codecasts.com.br/lesson');
+        // SEOTools::opengraph()->addProperty('type', 'articles');
+        // SEOTools::twitter()->setSite('@LuizVinicius73');
+        SEOTools::jsonLd()->addImage(config('app.url').'/images/resource/about-44.jpg');
+
+        OpenGraph::addImage(['url' => config('app.url').'/images/resource/about-44.jpg', 'size' => 300]);
+        OpenGraph::addImage(config('app.url').'/images/resource/about-44.jpg', ['height' => 300, 'width' => 300]);
+
 
 
         return view('index');
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,6 +8,8 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        {!! SEO::generate() !!}
         <!--====== Title ======-->
         <title>Jafro Foods Limited</title>
         <!--====== Favicon Icon ======-->
